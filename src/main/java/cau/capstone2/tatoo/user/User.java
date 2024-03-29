64a2b313d500561d802c00f4fac0be2deac092b5
pjw-redt <pjiww4@gmail.com>
2024-03-29 20:37:12 +0900

    [Domain] user setting

--- src/main/java/cau/capstone2/tatoo/user/User.java
+++ src/main/java/cau/capstone2/tatoo/user/User.java
@@ -1,12 +1,27 @@
 package cau.capstone2.tatoo.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Entity
 @Getter
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
+
+    private boolean isUsed = false;
+
+    private String nickname;
+
+    public static User createUser(String nickname) {
+        User user = new User();
+        user.nickname = nickname;
+        return user;
+    }
+
+    public void updateUserState() {
+        this.isUsed = true;
+    }
 }
