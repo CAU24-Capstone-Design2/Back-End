2f571f365f8b39fce6049c139cd0e46a3adb3ed7
pjw-redt <pjiww4@gmail.com>
2024-03-30 16:45:51 +0900

    [feat] 버전 변경으로 인한 bean 설정 추가

--- src/main/java/cau/capstone2/tatoo/TatooApplication.java
+++ src/main/java/cau/capstone2/tatoo/TatooApplication.java
@@ -2,12 +2,19 @@ package cau.capstone2.tatoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class TatooApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TatooApplication.class, args);
 	}
 
+	@Bean
+	public WebClient webClient() {
+		return WebClient.builder().build();
+	}
 }
