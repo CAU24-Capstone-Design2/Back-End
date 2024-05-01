f7adf001571a7a8e38ff191c3c2660ce8bc6e414
pjw-redt <pjiww4@gmail.com>
2024-05-01 13:36:21 +0900

    [feat] save를 위한 repository 구현'

---
+++ src/main/java/cau/capstone2/tatoo/scar/repository/ScarRepository.java
@@ -0,0 +1,7 @@
+package cau.capstone2.tatoo.scar.repository;
+
+import cau.capstone2.tatoo.scar.domain.Scar;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScarRepository extends JpaRepository<Scar, Long> {
+}
