bb86f7802fde566bb7d5e64d4e7fb4d9e0827270
pjw-redt <pjiww4@gmail.com>
2024-05-02 16:18:26 +0900

    [feat] setter 제거 및 tattoo 설정 추가

--- src/main/java/cau/capstone2/tatoo/scar/domain/Scar.java
+++ src/main/java/cau/capstone2/tatoo/scar/domain/Scar.java
@@ -1,19 +1,13 @@
 package cau.capstone2.tatoo.scar.domain;
 
 
-import cau.capstone2.tatoo.tattoo.domain.Tattoo;
 import cau.capstone2.tatoo.user.domain.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
-@Setter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Scar {
@@ -26,9 +20,14 @@ public class Scar {
 
     private String scarSegUri; //scar segmentation image filePath
 
+    //AI 도안
+    private String designUri; //tattoo design image filePath
+
     private String scarImage; //s3 - scarImage
 
     private String scarSegImage; //s3 - scarSegImage
+
+    private String tattooImage; //s3 - tattoo image
 
     private String scarKeyWord;
 
@@ -38,13 +37,27 @@ public class Scar {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToMany(mappedBy = "scar", cascade = CascadeType.REMOVE)
-    private List<Tattoo> tatooList = new ArrayList<>();
-
     public static Scar createScar(String scarDescription, String scarKeyWord) {
         Scar scar = new Scar();
         scar.scarDescription = scarDescription;
         scar.scarKeyWord = scarKeyWord;
         return scar;
     }
+
+    public void setScarImage(String scarImage) {
+        this.scarImage = scarImage;
+    }
+
+    public void setScarUri(String scarUri) {
+        this.scarUri = scarUri;
+    }
+
+    public void setScarSegImage(String scarSegImage) {
+        this.scarSegImage = scarSegImage;
+    }
+
+    public void setTattooImage(String tattooImage) {
+        this.tattooImage = tattooImage;
+    }
+
 }
