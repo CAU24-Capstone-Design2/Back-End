c1702ade6f798bd86293959c4433c7e04cc504f3
pjw-redt <pjiww4@gmail.com>
2024-05-02 16:19:00 +0900

    [feat] userId로 scar반환 리스트 추가

--- src/main/java/cau/capstone2/tatoo/scar/repository/ScarRepository.java
+++ src/main/java/cau/capstone2/tatoo/scar/repository/ScarRepository.java
@@ -3,5 +3,9 @@ package cau.capstone2.tatoo.scar.repository;
 import cau.capstone2.tatoo.scar.domain.Scar;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScarRepository extends JpaRepository<Scar, Long> {
+
+    List<Scar> findAllByUserId(Long userId);
 }
