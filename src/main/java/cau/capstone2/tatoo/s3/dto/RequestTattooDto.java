4f0c7a4bb37e85d6e2da664fa025710ded3ef1b6
pjw-redt <pjiww4@gmail.com>
2024-05-02 16:21:36 +0900

    [feat] RequestTattooDto 구현

---
+++ src/main/java/cau/capstone2/tatoo/s3/dto/RequestTattooDto.java
@@ -0,0 +1,17 @@
+package cau.capstone2.tatoo.s3.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestTattooDto {
+    private MultipartFile scarImage;
+    private String styleKeyWord;
+    private String styleDescription;
+
+}
